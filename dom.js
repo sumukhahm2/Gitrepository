7fcfdf2d836925ec57c42b32327c7442412639a3
sumukhahm2 <sumukhahm2@gmail.com>
2023-06-21 22:17:20 +0530

    nth commit

--- dom.js
+++ dom.js
@@ -1,15 +1,32 @@
-//var items=document.getElementById('items');
-//var headertitle=document.getElementById('list-group-item');
-
-//headertitle.style.border='solid black';
-var itemss=document.getElementsByClassName('list-group-item');
-itemss[1].style.backgroundColor='green';
-itemss[2].style.display='none';
-var lists=document.querySelectorAll('.list-group-item:nth-child(2)');
-lists[0].style.color='green';
-var list=document.querySelectorAll('li:nth-child(odd)');
-for(var i=0;i<list.length;i++)
-{
-    list[i].style.backgroundColor='green';
-}
-
+var itemlist=document.querySelector('#items');
+//console.log(itemlist.parentElement);
+//console.log(itemlist.parentNode);
+//itemlist.parentNode.style.background='red';
+//console.log(itemlist.lastElementChild)
+//itemlist.lastElementChild.textContent='hello';
+//console.log(itemlist.lastChild)
+var elem=document.createElement('li');
+elem.className='helloword';
+var text=document.createTextNode('hello world');
+elem.appendChild(text);
+var addlist=document.querySelector('#items');
+console.log(addlist)
+var lastlist=document.querySelectorAll('li')[0];
+console.log(lastlist)
+addlist.insertBefore(elem,lastlist);
+console.log(elem);
+//console.log(itemlist.firstElementChild)
+//console.log(itemlist.firstChild)
+//console.log(itemlist.nextSibling)
+//console.log(itemlist.nextElementSibling)
+//console.log(itemlist.previousElementSibling)
+//console.log(itemlist.previousSibling)
+//var addition=document.createElement('div')
+//addition.className='hello';
+//addition.id='hello';
+//addition.setAttribute('title','hello world')
+//var prev=document.querySelector('header .container')
+//var next=document.querySelector('header h1')
+//var txt=document.createTextNode('hello world')
+//addition.appendChild(txt)
+//prev.insertBefore(addition,next)
