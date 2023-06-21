ddb9ae32677220be003da94fe0d2cec3cc5a46b3
sumukhahm2 <sumukhahm2@gmail.com>
2023-06-21 08:26:38 +0530

    second commit

---
+++ dom.js
@@ -0,0 +1,10 @@
+//var items=document.getElementById('items');
+//var headertitle=document.getElementById('list-group-item');
+
+//headertitle.style.border='solid black';
+var itemss=document.getElementsByClassName('list-group-item');
+for(var i=0;i<itemss.length;i++)
+{
+    itemss[i].style.fontWeight='bold';
+    itemss[i].style.color='green';
+}
