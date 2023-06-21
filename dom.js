ceee06885143c400685a0810d113b14cf329ed43
sumukhahm2 <sumukhahm2@gmail.com>
2023-06-21 11:30:13 +0530

    fifth commit

--- dom.js
+++ dom.js
@@ -2,9 +2,14 @@
 //var headertitle=document.getElementById('list-group-item');
 
 //headertitle.style.border='solid black';
-var itemss=document.getElementsByClassName('list-group-item5');
-console.log(itemss)
-itemss[0].style.background='yellow';
-var li=document.getElementsByTagName('li');
-console.log(li)
-li[4].style.fontWeight='bold';
+var itemss=document.getElementsByClassName('list-group-item');
+itemss[1].style.backgroundColor='green';
+itemss[2].style.display='none';
+var lists=document.querySelectorAll('.list-group-item:nth-child(2)');
+lists[0].style.color='green';
+var list=document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<list.length;i++)
+{
+    list[i].style.backgroundColor='green';
+}
+
