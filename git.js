6ac137909f1502e196fec75aa64c0c4a3cde1e1e
sumukhahm2 <137076103+sumukhahm2@users.noreply.github.com>
2023-07-11 15:48:21 +0530

    Update git.js

--- git.js
+++ git.js
@@ -1,1 +1,32 @@
-console.log('I  love git');
+var posts=[];
+
+
+function updateLastUserActivityTime(){
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+           posts.ActivityTime=new Date().getTime();
+           resolve( posts.ActivityTime)
+        },1000)
+    })
+    
+    
+}
+
+function createPost(post){
+    return new Promise((resolve)=>{
+    setTimeout(()=>{
+        posts.push(post)
+     resolve(post);
+     
+    },2000)
+})
+}
+//createPost({Post:'post1',ActivityTime:new Date().getTime()})
+//createPost({Post:'post2',ActivityTime:new Date().getTime()})
+
+function getPost(){
+    Promise.all([createPost({title: 'Post Five', body: 'This is Post Five'}), updateLastUserActivityTime()]).then(([createPostresolve,updateLastUserActivityTimeresolve])=>{
+        console.log(createPostresolve+' '+updateLastUserActivityTimeresolve)
+    })
+}
+getPost();
